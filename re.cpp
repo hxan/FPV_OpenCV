47c4ab5086eeaba812bbc242b332c0136fda49ae
hxan <59756540+hxan@users.noreply.github.com>
2020-10-09 13:55:07 +0800

    Add files via upload

--- re.cpp
+++ re.cpp
@@ -1,13 +1,11 @@
-
-#include<opencv2/opencv.hpp>
-#include<opencv2/imgproc.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/tracking.hpp>
-#include<opencv2/core/ocl.hpp>
-#include<stdio.h>
-#include<stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <stdio.h>
 #include <sys/socket.h>
-#include<string.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
@@ -24,32 +22,46 @@ void camera_fun(int port)
 	/*
 		port为笔记本接收端口,注意和工控机发送端口保持一zhi
 	*/
-
 	struct sockaddr_in addrRec;
 	addrRec.sin_family = AF_INET;
 	addrRec.sin_port = htons(port);//port
 	addrRec.sin_addr.s_addr = inet_addr("127.0.0.1");//ip
+	
 	int sockClient = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockClient < 0){
+		printf("%d create socket fail\n",port);
+		return ;
+	}
 
-	bind(sockClient, (struct sockaddr*)&addrRec, sizeof(sockaddr_in));
+	int ret_bind = bind( sockClient, (struct sockaddr*)&addrRec, sizeof(sockaddr_in) );
+    if(ret_bind < 0){
+		printf("%d bind fail\n",port);
+		return ;
+	}
 
 	struct sockaddr_in addrSend;
 	int nLen = sizeof(struct sockaddr_in);
-		
+	int count;	
 	Mat frame;
 		
 	while (1)
 	{
 		std::vector<uchar> buff(65546);
-		recvfrom(sockClient,&buff[0],65546, 0, NULL,NULL);//接收压缩的数据
+		count = recvfrom(sockClient,&buff[0],65546, 0, NULL,NULL);//接收压缩的数据
+		if(count == -1){
+			printf("recieve data fail!");
+			waitKey(10);
+			continue;
 
-		Mat frame= cv::imdecode(buff, -1);//解压
+		}
+		Mat frame = imdecode(buff, -1);//解压
 		imshow("receive", frame);//显示
 		waitKey(1);
 	}	
 
-}
+	close(sockClient);
 
+}
 
 int main(int argc,char **argv)
 {
--- se.cpp
+++ se.cpp
@@ -1,52 +1,63 @@
-#include<opencv2/opencv.hpp>
-#include<opencv2/imgproc.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/tracking.hpp>
-#include<opencv2/core/ocl.hpp>
-#include<stdio.h>
-#include<stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <stdio.h>
 #include <sys/socket.h>
-#include<string.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include<time.h>
+#include <time.h>
 using namespace cv;
 
 void camera_fun(char ip[],int port ,int device,int quality){
 
-	int sum=0;
 	struct sockaddr_in addrRec;
+
 	addrRec.sin_family = AF_INET;
 	addrRec.sin_port = htons(port);//
 	addrRec.sin_addr.s_addr = inet_addr(ip);//
+	
 	int sockClient = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if(sockClient < 0){
+		printf("%d create socket fail\n",port);
+		return ;
+	}
+
 	int nLen = sizeof(sockaddr_in);
  
-
-	cv::VideoCapture capture(device);
-	cv::Mat capframe;
+	VideoCapture capture(device);
+	Mat capframe;
+	
 	std::vector<int> params;
-	params.push_back(cv::IMWRITE_JPEG_QUALITY);
+	params.push_back(IMWRITE_JPEG_QUALITY);
 	params.push_back(quality);
+
 	while (1)
 	{
 		capture >> capframe;
 		std::vector<uchar> buff;
-		cv::imencode(".jpg", capframe, buff, params);
-		Mat frame= cv::imdecode(buff, -1);
+		imencode(".jpg", capframe, buff, params);
+	
+		Mat frame = imdecode(buff, -1);
 		sendto(sockClient, (char *)&buff[0],buff.size() , 0, (struct sockaddr*)&addrRec, nLen);
 		waitKey(10);
 	}
+
+
+	close(sockClient);
 }
 int main(int argc,char **argv)
 {
 	char *ip=argv[1];
-	int port = atoi(argv[2] );
-
-	int device= atoi(argv[3]);
-	int quality=atoi(argv[4]);
-	
+	int port = atoi(argv[2]);
+	int device = atoi(argv[3]);
+	int quality = atoi(argv[4]);
+	 
 	camera_fun(ip,port,device,quality);
+
     return 0;
 }
